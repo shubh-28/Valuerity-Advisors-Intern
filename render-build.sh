2c5b592f9f727ce8dae2145ccf5f69f1068c6864
shubh-28 <shubh28saxena@gmail.com>
2025-01-03 20:23:20 +0530

    Update render-build.sh with SDL2 dependencies

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Update package lists
 apt-get update
-apt-get install -y build-essential cmake libopenblas-dev liblapack-dev python3-dev
+
+# Install build tools and dependencies
+apt-get install -y \
+    build-essential \
+    cmake \
+    libopenblas-dev \
+    liblapack-dev \
+    python3-dev \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev
